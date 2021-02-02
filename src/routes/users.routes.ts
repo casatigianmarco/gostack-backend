f873dbe1ed12ffa6a486da78239fafcdf117f421
Gianmarco <gianmarcocasati1@gmail.com>
2021-02-02 01:05:04 -0300

    avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -13,5 +18,23 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json(err.message);
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateAvatarService = new UpdateUserAvatarService();
+      const result = await updateAvatarService.execute({
+        user_id: request.user.id,
+        avatar_filename: request.file.filename,
+      });
+      return response.json(result);
+    } catch (err) {
+      return response.status(400).json(err.message);
+    }
+  },
+);
 
 export default usersRouter;
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,48 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  user_id: string;
+  avatar_filename: string;
+}
+interface Response {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
+class UpdateUserAvatarService {
+  public async execute({
+    user_id,
+    avatar_filename,
+  }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('User not exists.');
+    }
+
+    if (user.avatar) {
+      // delete avatar
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+    user.avatar = avatar_filename;
+    await usersRepository.save(user);
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    };
+  }
+}
+
+export default UpdateUserAvatarService;
